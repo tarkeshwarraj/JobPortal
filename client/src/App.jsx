9757e7896ff158c73e791f96215319b68063f5c1
tarkeshwar raj <tarkeshwarraj@gmail.com>
2025-01-27 18:55:38 +0530

    pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/student/Home'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-       <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+       <Routes>
+          <Route path='/' element={<Home />} />
+       </Routes>
     </>
   )
 }
