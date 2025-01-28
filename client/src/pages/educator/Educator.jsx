4ee267ecfea46c078d376596bf74170a68bda989
tarkeshwar raj <tarkeshwarraj@gmail.com>
2025-01-28 17:43:48 +0530

    navbar

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Educator = () => {
   return (
-    <div>Educator</div>
+    <div>
+      <h1>Educator page</h1>
+      {<Outlet/>}
+    </div>
   )
 }
 
